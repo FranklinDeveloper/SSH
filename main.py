c2c132fd64cced81a8a325ec51f684d8b36414b2
FranklinTadeu <12633796+FranklinTadeu@users.noreply.github.com>
2025-07-07 20:36:04 -0300

    Atualização automática

---
+++ main.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MeuApp(App):
+    def build(self):
+        return Label(text='Olá, mundo!')
+
+if __name__ == '__main__':
+    MeuApp().run()
